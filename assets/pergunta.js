5d02aceb2773e890ae4184a5d44104140566fc3e
netinhobabinski <159185041+netinhobabinski@users.noreply.github.com>
2024-10-01 16:40:24 -0300

    01/10
    .

---
+++ assets/pergunta.js
@@ -0,0 +1,46 @@
+criarCartao(
+    'Programaçao',
+    'O que python',
+    'O python é uma linguagem de programação',
+
+)
+
+criarCartao(
+    'Geografia',
+    'Qual capital da frança é paris',
+
+)
+
+criarCartao(
+    'Programação',
+    'O que é uma função',
+    'Uma função é m bloco de código que executa alguma tarefa',
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
