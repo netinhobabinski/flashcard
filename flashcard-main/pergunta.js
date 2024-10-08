d116a66b43c56deadbf12567add000ba3b1fc201
netinhobabinski <159185041+netinhobabinski@users.noreply.github.com>
2024-10-08 16:51:00 -0300

    08/10
    .

---
+++ flashcard-main/pergunta.js
@@ -0,0 +1,46 @@
+criarCartao(
+    'Matematica',
+    'Quanto é 4.8?',
+    '32',
+
+)
+
+criarCartao(
+    'Geografia',
+    'Qual capital da frança é paris',
+
+)
+
+criarCartao(
+    'futebol',
+    'Qual é o time que ta em primeiro na tabela do brasileirão',
+    'Botafogo',
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
